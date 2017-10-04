d31e830efda24db82896a32b69c84443b4c7ace6
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2017-10-04 18:12:00 -0500

    [IMP] tree view

--- models/cost_in_dollars_report.py
+++ models/cost_in_dollars_report.py
@@ -11,21 +11,24 @@ class CostinDollarsReport(models.TransientModel):
     apportionments = fields.Many2many(
         comodel_name='product.product'
     )
+    name = fields.Datetime(
+        default=fields.Datetime.now(),
+    )
 
     @api.one
     def create_report(self):
-        landed_costs = self.env['stock.landed.cost'].search([('state', '=', 'done')]).sorted(key=lambda r: r.date)  # TODO Only done?
+        landed_costs = self.env['stock.landed.cost'].search([('state', '=', 'done')]).sorted(key=lambda r: r.date)
         products = self.env['product.product'].search([])
         for product in products:
-            new_product = False
+            new_product = True
             for landed_cost in landed_costs:
+                if not new_product:
+                    break
                 for valuation_adjustment_line in landed_cost.valuation_adjustment_lines:
-                    if new_product:
-                        continue
                     if product == valuation_adjustment_line.product_id:
+                        new_product = False
                         line = self.env['cost.dlls.report.line'].search([('report', '=', self.id), ('product', '=', product.id)])
                         if not line:
-                            new_product = True
                             line_dict = {
                                 'product': product.id,
                                 'landed_cost': landed_cost.id,
@@ -41,12 +44,13 @@ class CostinDollarsReport(models.TransientModel):
                             'product': valuation_adjustment_line.cost_line_id.product_id.id,
                             'additional_landed_cost': valuation_adjustment_line.additional_landed_cost,
                         }).line = line
-        for apportionment in self.apportionments:
-            for line in self.lines:
-                if apportionment not in [a.product for a in line.apportionments]:
-                    self.env['cost.dlls.report.line.apportionment'].create({
-                        'product': apportionment.id,
-                    }).line = line
-        self.apportionments = self.apportionments.sorted(key=lambda r: r.name)
-        for line in self.lines:
-            line.apportionments = line.apportionments.sorted(key=lambda r: r.product.name)
+        # return self.print_report()
+        # for apportionment in self.apportionments:
+        #     for line in self.lines:
+        #         if apportionment not in [a.product for a in line.apportionments]:
+        #             self.env['cost.dlls.report.line.apportionment'].create({
+        #                 'product': apportionment.id,
+        #             }).line = line
+        # self.apportionments = self.apportionments.sorted(key=lambda r: r.name)
+        # for line in self.lines:
+        #     line.apportionments = line.apportionments.sorted(key=lambda r: r.product.name)
